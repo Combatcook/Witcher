fa42ed54144cbb1f425a4c2ed0ded167f36424fc
Povarov <cook0208@mail.ru>
2020-03-14 18:22:08 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 var mySwiper = new Swiper('.swiper-container', {
-    slidesPerView: 2,
+    slidesPerView: 1,
     loop: true,
     navigation: {
         nextEl: '.arrow',
       },
+    breakpoints: {
+      540: {
+        slidesPerView: 2
+      }
+    }
 });
+
+var menuMobile = document.querySelector('.menu-mobile');
+var menu = document.querySelector('.header');
+
+menuMobile.addEventListener('click', function () {
+  menuMobile.classList.toggle('menu-mobile-active')
+  menu.classList.toggle('header-active');
+})
